cd129eca720cff3874cc7d4b21764e8656b8c4d6
HMADA ALSFAH <hmadaalsfah1@gmail.com>
2025-12-29 17:39:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4913146948-5ce58",
+  "appId": "1:903504634418:web:94385da8663dadbb61122b",
+  "apiKey": "AIzaSyCfVFyAn2QJ5z6YgBczVjtI87wgFrdkszw",
+  "authDomain": "studio-4913146948-5ce58.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "903504634418"
+};
