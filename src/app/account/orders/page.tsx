b2860cd343f1979da9d977c2490f1a7ac8cd1629
HMADA ALSFAH <hmadaalsfah1@gmail.com>
2025-12-30 11:23:08 +0000

    Try fixing this error: `Runtime FirebaseError: Missing or insufficient p

--- src/app/account/orders/page.tsx
+++ src/app/account/orders/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useUser, useFirestore, useCollection, useMemoFirebase } from '@/firebase';
